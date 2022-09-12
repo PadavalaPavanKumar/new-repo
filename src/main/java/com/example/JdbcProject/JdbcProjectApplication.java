860296265a9369bd98dcc3a32978e97897465685
PadavalaPavanKumar <pavankumar.padavala@kanerika.com>
2022-09-12 15:00:19 +0530

    1st commit

---
+++ src/main/java/com/example/JdbcProject/JdbcProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.JdbcProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class JdbcProjectApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(JdbcProjectApplication.class, args);
+		
+	}
+
+}
